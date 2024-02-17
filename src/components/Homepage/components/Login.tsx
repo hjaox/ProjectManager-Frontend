fb08e12cdc59e10f3a23f79f7c94680b38947021
hjaox <hectorjana.o@gmail.com>
2024-02-17 13:19:23 +0000

    [FEAT]added Login form

--- src/components/Homepage/components/Login.tsx
+++ src/components/Homepage/components/Login.tsx
@@ -1,3 +1,38 @@
+import { useState } from "react"
+
 export default function Login() {
-    return <p>Login</p>
+    let [username, setUsername] = useState("");
+    let [password, setPassword] = useState("");
+
+    function handleUsernameInput(event: React.ChangeEvent<HTMLInputElement>): void {
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordInput(event : React.ChangeEvent<HTMLInputElement>): void {
+        setPassword(event.target.value);
+    }
+
+    function handleSubmit(event : React.FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        console.log(username, password);
+    }
+
+    return (
+        <form id="loginForm"
+        onSubmit={event => handleSubmit(event)}>
+            <input name="usernameInput"
+            id="usernameInput"
+            type="text"
+            onChange={event => handleUsernameInput(event)} />
+            <input name="passwordInput"
+            id="passwordInput"
+            type="text"
+            onChange={event => handlePasswordInput(event)} />
+            <button
+            type="submit"
+            form="loginForm">
+                Login
+            </button>
+        </form>
+    )
 }
